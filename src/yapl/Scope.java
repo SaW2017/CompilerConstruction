4963620ee7bfcfa63e56a1cc54cad440e8fbfa97
leusl <dleustik@hotmail.com>
2020-05-01 15:37:06 +0200

    Scope final implementation

--- src/yapl/Scope.java
+++ src/yapl/Scope.java
@@ -12,6 +12,29 @@ public class Scope {
     public Scope(boolean isGlobal, Symbol parentSymbol, HashMap<String, Symbol> symbols) {
         this.isGlobal = isGlobal;
         this.parentSymbol = parentSymbol;
+    }
+
+    public boolean isGlobal() {
+        return isGlobal;
+    }
+
+    public void setGlobal(boolean global) {
+        isGlobal = global;
+    }
+
+    public Symbol getParentSymbol() {
+        return parentSymbol;
+    }
+
+    public void setParentSymbol(Symbol parentSymbol) {
+        this.parentSymbol = parentSymbol;
+    }
+
+    public HashMap<String, Symbol> getSymbols() {
+        return symbols;
+    }
+
+    public void setSymbols(HashMap<String, Symbol> symbols) {
         this.symbols = symbols;
     }
 }
