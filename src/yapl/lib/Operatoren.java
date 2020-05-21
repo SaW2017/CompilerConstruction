6694c2d99f67fd0c3f72869cc5db15bdad10f55d
SaW2017 <s1wachter@edu.aau.at>
2020-05-21 19:17:58 +0200

    Create Operatoren.java
    Operatoren hinzugefuegt

---
+++ src/yapl/lib/Operatoren.java
@@ -0,0 +1,30 @@
+package yapl.lib;
+
+public class Operatoren {
+
+//    Arithmetic Oper
+    public static final String Add       = "+";
+    public static final String Sub       = "-";
+    public static final String Mult      = "*";
+    public static final String Div       = "/";
+    public static final String Mod       = "%";
+
+    public static final String[] ArithmeticOps = {Add, Sub, Mult, Div, Mod};
+
+
+//    equalExp
+    public static final String Equal            = "==";
+    public static final String Unequal          = "!=";
+    public static final String GreaterThan      = ">";
+    public static final String SmallerThan      = "<";
+    public static final String GreaterOrEqual   = ">=";
+    public static final String LessOrEqual      = "<=";
+
+    public static final String[] EqualExp = {Equal, Unequal, GreaterThan, SmallerThan, GreaterOrEqual, LessOrEqual};
+
+//    booleOp
+    public static final String And  = "And";
+    public static final String Or   = "Or";
+
+    public static final String[] BinaryExp = {Add, Sub, Mult, Div, Mod, Equal, Unequal, GreaterThan, SmallerThan, GreaterOrEqual, LessOrEqual, And, Or};
+}
